b349eb3912e27049951f1bdc1360a71dff3f50b5
Raghu S <40663555+raghu04@users.noreply.github.com>
2026-02-07 21:03:05 +0530

    claude code optimization (#2)
    * claude code optimization
    
    * reformat all files according to black's style guide
    
    * Exclude your .venv directory from linting
    
    * removed code formatters from CI workflow
    
    * removed headless execution from CI workflow
    
    * gradio UI use cases
    
    * code refactor
    
    * text summarization using hf model and radio ui
    
    * export the summarized test into a file

--- Basecamp/Day_1/Data_Pack/Data_Processing.py
+++ Basecamp/Day_1/Data_Pack/Data_Processing.py
@@ -2,9 +2,10 @@
 # These functions are here just to represent how module is constructed
 
 # Global scope for this variable
-Module_Version = '0.1'
+Module_Version = "0.1"
 
-def Process_Data (p1, p2):
+
+def Process_Data(p1, p2):
     """
     This function applies a constant factor on the agruments and returns them.
     If there are errors, it returns error message
@@ -18,22 +19,24 @@ def Process_Data (p1, p2):
         r1 = p1 * fac
         r2 = p2 * fac
 
-        print (f"Processed data from module version {Module_Version}. The values are {r1}, {r2}")
+        print(
+            f"Processed data from module version {Module_Version}. The values are {r1}, {r2}"
+        )
 
         return r1, r2
-    
+
     except Exception as e:
 
         return f"Error occured : {str(e)}"
 
-def Print_Data (p1, p2) :
-    
+
+def Print_Data(p1, p2):
     """
     This function just prints without any process
     """
 
     try:
-        print ('The data parameters are : ' + str(p1) + " " + str(p2))
-    
+        print("The data parameters are : " + str(p1) + " " + str(p2))
+
     except Exception:
         pass
